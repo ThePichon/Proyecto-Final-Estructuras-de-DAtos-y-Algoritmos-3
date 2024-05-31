0332d99847123fa0642ab5b944c8dc9724ee1e36
ThePichon <165832792+ThePichon@users.noreply.github.com>
2024-05-31 16:22:02 -0600

    Update Bot_Random.py
    Ponle si mijo

--- Main/Bot_Random.py
+++ Main/Bot_Random.py
@@ -1,1 +1,10 @@
 # Pichon Aqui va el bot random que vamos a usar para probarlo
+import random
+import game
+
+class BotRandom:
+    def make_move(self, board, player):
+        # Elige un movimiento al azar, pero asegúrate de que sea válido
+        valid_moves = game.valid_moves(board)
+        return random.choice(valid_moves)
+#Aqui esta padrino, el codigo mas complicado que eh echo 
